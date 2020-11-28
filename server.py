e38d75cf6dcab81a8e7b0d94133b36c869c91a39
teslaistra <danyefimoff@gmail.com>
2020-11-28 23:33:39 +0300

    docs for server

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-print("stat")
-
 from fastapi import FastAPI
 from SQLighter import *
 from text_model.text_model import TextModel
@@ -12,9 +10,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-# Логин в приложение через внутренний аккаунт
 @app.get("/login/")
 async def login(login: str, password: str):
+    """
+    Логин в приложение через внутренний аккаунт
+    :param login: Логин пользователя
+    :param password: Пароль пользователя
+    :return: результат логина
+    """
     db_worker = SQLighter("hell.db")
 
     is_registred = db_worker.get_user(login, password)
@@ -24,9 +27,14 @@ async def login(login: str, password: str):
     return result
 
 
-# Получение списка опросов
+#
 @app.get("/top_survies/")
 async def read_coords(user_id: float):
+    """
+    Получение списка опросов
+    :param user_id: номер пользователя
+    :return: список рубрик(опросов)
+    """
     db_worker = SQLighter("hell.db")
     survey = db_worker.get_survies(user_id)
     result = {}
@@ -38,33 +46,50 @@ async def read_coords(user_id: float):
     return result
 
 
-# Получение списка инициатив
 @app.get("/top_initiatives/")
-async def read_coords():
+async def top_initiatives():
+    """
+    Получение списка инициатив
+    :return: вернет список инициатив
+    """
     db_worker = SQLighter("hell.db")
     initiatives = db_worker.get_initiatives()
 
     return initiatives
 
 
-# Отправка жалобы
 @app.get("/send_complaint/")
 async def send_complaint(text: str, user_id: float):
+    """
+    Отправка жалобы
+    :param text: Текст жалобы
+    :param user_id: Номер пользователя
+    """
     db_worker = SQLighter("hell.db")
     db_worker.send_complaint(text, user_id)
     db_worker.close()
 
 
-# Получить вопросы в конкретной рубрике
+#
 @app.get("/get_questions/")
 async def get_questions(rubric_id: int):
+    """
+    Получить вопросы в конкретной рубрике
+    :param rubric_id: Номер рубрики
+    :return: Список вопросов из этой рубрики
+    """
     db_worker = SQLighter("hell.db")
     return db_worker.get_questions(rubric_id)
 
 
-# Отправка ответа в свободной форме на вопрос
 @app.get("/send_free_answer/")
 async def send_free_answer(text: str, question_id: int, user_id: int):
+    """
+    Отправка ответа в свободной форме на вопрос
+    :param text: Текст ответа
+    :param question_id: Номер вопроса
+    :param user_id: Номер пользователя
+    """
     db_worker = SQLighter("hell.db")
 
     predicter = TextModel()
@@ -74,9 +99,15 @@ async def send_free_answer(text: str, question_id: int, user_id: int):
     db_worker.send_free_answer(text, question_id, result_prediction, user_id)
     db_worker.close()
 
-# Отправка ответа в виде варианта ответа на вопрос
+
 @app.get("/send_variant_answer/")
 async def send_variant_answer(variant_id: str, question_id: int, user_id: int):
+    """
+    Отправка ответа в виде варианта ответа на вопрос
+    :param variant_id: Номер варианта ответа
+    :param question_id: Номер вопроса
+    :param user_id: Номер пользователяд
+    """
     db_worker = SQLighter("hell.db")
 
     db_worker.send_variant_answer(variant_id, question_id, user_id)
